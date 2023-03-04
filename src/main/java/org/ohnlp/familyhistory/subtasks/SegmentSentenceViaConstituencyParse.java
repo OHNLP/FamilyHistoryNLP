2e71e0b335755dd11245cd70f95234088b54d552
Andrew Wen <2060andrew@gmail.com>
2023-03-04 08:27:16 -0600

    Add Sentence Segmentation via OpenNLP, Increment version for release

--- src/main/java/org/ohnlp/familyhistory/subtasks/SegmentSentenceViaConstituencyParse.java
+++ src/main/java/org/ohnlp/familyhistory/subtasks/SegmentSentenceViaConstituencyParse.java
@@ -1,5 +1,11 @@
 package org.ohnlp.familyhistory.subtasks;
 
+import opennlp.tools.cmdline.parser.ParserTool;
+import opennlp.tools.parser.Parse;
+import opennlp.tools.parser.Parser;
+import opennlp.tools.parser.ParserFactory;
+import opennlp.tools.parser.ParserModel;
+import opennlp.tools.util.Span;
 import org.apache.beam.sdk.coders.KvCoder;
 import org.apache.beam.sdk.coders.RowCoder;
 import org.apache.beam.sdk.coders.StringUtf8Coder;
@@ -12,12 +18,14 @@ import org.apache.beam.sdk.values.KV;
 import org.apache.beam.sdk.values.PCollection;
 import org.apache.beam.sdk.values.Row;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.Comparator;
+import java.util.List;
 
 /**
- * This component performs segmentation, if desired. At present, this functionality is not present and as such only
- * re-outputs sentences in order
+ * Performs sentence segmentation via constituency parse, and retains second level segments (or top level, if no second level are found)
  */
 public class SegmentSentenceViaConstituencyParse extends PTransform<PCollection<Row>, PCollection<Row>> {
 
@@ -50,8 +58,12 @@ public class SegmentSentenceViaConstituencyParse extends PTransform<PCollection<
         ).apply(
                 "Generate Segments in Order By Document",
                 ParDo.of(new DoFn<KV<String, Iterable<Row>>, Row>() {
+                    private transient Parser parser;
                     @StartBundle
-                    public void init() {
+                    public void init() throws IOException {
+                        InputStream is = SegmentSentenceViaConstituencyParse.class.getResourceAsStream("/org/ohnlp/familyhistory/models/opennlp/en-parser-chunking.bin");
+                        ParserModel model = new ParserModel(is);
+                        this.parser = ParserFactory.create(model);
                     }
 
                     @ProcessElement
@@ -76,20 +88,90 @@ public class SegmentSentenceViaConstituencyParse extends PTransform<PCollection<
                         // Now do actual segmentation
                         int documentChunkCount = 0;
                         for (Row r : cleanedRows) {
-                            pc.output(
-                                    Row.withSchema(SCHEMA).addValues(
-                                            r.getString("document_id"),
-                                            r.getInt32("sentence_id"),
-                                            0,
-                                            documentChunkCount++,
-                                            r.getString("chunk_text"),
-                                            0,
-                                            r.getString("chunk_text").length()
-                                    ).build()
-                            );
+                            int sentenceChunkCount = 0;
+                            String base = r.getString("chunk_text");
+                            Parse[] constituencyTree = ParserTool.parseLine(base, this.parser, 1);
+                            // Parse out longest non-root S segments
+                            List<Span> longestNonRootSegments = getLongestNonRootSegments(constituencyTree[0]);
+                            if (longestNonRootSegments.size() == 0) {
+                                // Only the top S segment covering whole sentence exists
+                                longestNonRootSegments.add(
+                                        new Span(0, base.length())
+                                );
+                            }
+                            longestNonRootSegments.sort(Comparator.comparingInt(Span::getStart));
+                            int lastEnd = 0;
+                            for (int j = 0; j < longestNonRootSegments.size(); j++) {
+                                Span span = longestNonRootSegments.get(j);
+                                int begin = span.getStart();
+                                int end = span.getEnd();
+                                // First, check if we need to add a segment in between where last segment ended and the
+                                // beginning of this one
+                                if (lastEnd != begin) {
+                                    pc.output(
+                                            Row.withSchema(SCHEMA).addValues(
+                                                    r.getString("document_id"),
+                                                    r.getInt32("sentence_id"),
+                                                    sentenceChunkCount++,
+                                                    documentChunkCount++,
+                                                    base.substring(lastEnd, begin),
+                                                    lastEnd,
+                                                    begin
+                                            ).build()
+                                    );
+                                }
+                                // Output the regular span
+                                pc.output(
+                                        Row.withSchema(SCHEMA).addValues(
+                                                r.getString("document_id"),
+                                                r.getInt32("sentence_id"),
+                                                sentenceChunkCount++,
+                                                documentChunkCount++,
+                                                base.substring(begin, end),
+                                                begin,
+                                                end
+                                        ).build()
+                                );
+                                // Output trailing span, if necessary
+                                if (j == longestNonRootSegments.size() - 1 && end < base.length()) {
+                                    pc.output(
+                                            Row.withSchema(SCHEMA).addValues(
+                                                    r.getString("document_id"),
+                                                    r.getInt32("sentence_id"),
+                                                    sentenceChunkCount++,
+                                                    documentChunkCount++,
+                                                    base.substring(end),
+                                                    end,
+                                                    base.length()
+                                            ).build()
+                                    );
+                                }
+                                lastEnd = end;
+                            }
+
                         }
                     }
                 })
         );
+    }
+
+    private List<Span> getLongestNonRootSegments(Parse constituencyTree) {
+        List<Span> ret = new ArrayList<>();
+        Parse[] currLevel = constituencyTree.getChildren()[0].getChildren();
+        recursSearchFirstSegment(currLevel, ret);
+        return ret;
+    }
+
+    private void recursSearchFirstSegment(Parse[] currLevel, List<Span> ret) {
+        if (currLevel == null || currLevel.length == 0) {
+            return;
+        }
+        for (Parse child : currLevel) {
+            if (child.getType().equals("S")) {
+                ret.add(child.getSpan());
+            } else {
+                recursSearchFirstSegment(child.getChildren(), ret);
+            }
+        }
     }
 }
